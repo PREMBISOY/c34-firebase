36d59189c34e4ff8c190f07c8a3850e0a00a1796
PREMBISOY <77225599+PREMBISOY@users.noreply.github.com>
2021-04-02 15:07:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c34-firebase
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball, database;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    database=firebase.database();
+
+    var currPosOfBall = database.ref("ball/position");
+    currPosOfBall.on("value",readPosition);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref("ball/position").set({
+        'x': ball.x + x,
+        'y': ball.y + y
+    })
+}
+
+function readPosition(data){
+var currentPos = data.val();
+ball.x = currentPos.x;
+ball.y = currentPos.y
+
+}
